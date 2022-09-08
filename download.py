37fa30ab0103996d49f21f9e6e3a8b3cde33628c
lansejingyu <18710512504@163.com>
2022-09-08 15:50:12 +0800

    优化

--- download.py
+++ download.py
@@ -6,16 +6,13 @@
 # @Time : 2022/8/12 10:41
 
 
-
-
-
-
 import webbrowser as web
 import codecs
 import time
+
 with open('C:\\Users\\KT15\\Desktop\\666.txt') as fp:
-  for ebayno in fp:
-    url = ebayno.strip()
-    # time.sleep(1)
-    # print(url)
-    web.open(url)
+	for ebayno in fp:
+		url = ebayno.strip()
+		# time.sleep(1)
+		# print(url)
+		web.open(url)
